bc1fa8852b0ee8755f3c7020535cc93b0e311422
reggietee <79715267+reggietee@users.noreply.github.com>
2022-12-14 21:49:57 -0500

    v1 commit

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1615868.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 115006419, 115006427, 115006429, 115006443, 115006446, 115006453 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/115006412/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/115006413/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/115006411/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/115006416/1/basis.wasm.js', 'wasmUrl' : 'files/assets/115006417/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/115006415/1/basis.js', 'preload' : false},
+];
